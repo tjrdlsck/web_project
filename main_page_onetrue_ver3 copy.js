0604cba1829a8958e811d2397ecebe282b6a563e
tjrdlsck <61540707+tjrdlsck@users.noreply.github.com>
2024-06-01 05:47:03 +0900

    web_result

---
+++ main_page_onetrue_ver3 copy.js
@@ -0,0 +1,256 @@
+
+// 모달 열기
+function openModal1(clickedMenuId) {
+    const modalMenus = document.getElementsByClassName('modal_nav');
+    for (let i = 0; i < modalMenus.length; i++) {
+        modalMenus[i].style.textDecorationLine = 'none';
+    }
+    const modalContents = document.querySelectorAll('.v-content');
+    modalContents.forEach(content => {
+        content.style.display = 'none';
+    });
+    document.getElementById("myModal1").style.display = "block"; /* 모달 창을 보이게 합니다. */
+    document.getElementById("modal_nav1_1").style.textDecorationLine = 'underline';
+    document.getElementById("modal_nav1_1").style.textUnderlineOffset = '6px';
+    document.getElementById("modal_nav1_1").style.textDecorationThickness = '8px';
+    document.getElementById("modalMenusContent1_1").style.display = 'block';
+    document.body.style.overflow = 'hidden'; // 배경 페이지 스크롤 막기
+    
+}
+
+// 모달 닫기
+function closeModal1() {
+    document.getElementById("myModal1").style.display = "none"; /* 모달 창을 숨깁니다. */
+    document.body.style.overflow = 'auto'; // 배경 페이지 스크롤 허용
+}
+
+// 모달 열기
+function openModal2(clickedMenuId) {
+    const modalMenus = document.getElementsByClassName('modal_nav');
+    for (let i = 0; i < modalMenus.length; i++) {
+        modalMenus[i].style.textDecorationLine = 'none';
+    }
+    const modalContents = document.querySelectorAll('.v-content');
+    modalContents.forEach(content => {
+        content.style.display = 'none';
+    });
+    document.getElementById("myModal2").style.display = "block"; /* 모달 창을 보이게 합니다. */
+    document.getElementById("modal_nav2_1").style.textDecorationLine = 'underline';
+    document.getElementById("modal_nav2_1").style.textUnderlineOffset = '6px';
+    document.getElementById("modal_nav2_1").style.textDecorationThickness = '8px';
+    document.getElementById("modalMenusContent2_1").style.display = 'block';
+    document.body.style.overflow = 'hidden'; // 배경 페이지 스크롤 막기
+}
+
+// 모달 닫기
+function closeModal2() {
+    document.getElementById("myModal2").style.display = "none"; /* 모달 창을 숨깁니다. */
+    document.body.style.overflow = 'auto'; // 배경 페이지 스크롤 허용
+}
+// 모달 열기
+function openModal3(clickedMenuId) {
+    const modalMenus = document.getElementsByClassName('modal_nav');
+    for (let i = 0; i < modalMenus.length; i++) {
+        modalMenus[i].style.textDecorationLine = 'none';
+    }
+    const modalContents = document.querySelectorAll('.v-content');
+    modalContents.forEach(content => {
+        content.style.display = 'none';
+    });
+    document.getElementById("myModal3").style.display = "block"; /* 모달 창을 보이게 합니다. */
+    document.getElementById("modal_nav3_1").style.textDecorationLine = 'underline';
+    document.getElementById("modal_nav3_1").style.textUnderlineOffset = '6px';
+    document.getElementById("modal_nav3_1").style.textDecorationThickness = '8px';
+    document.getElementById("modalMenusContent3_1").style.display = 'block';
+    document.body.style.overflow = 'hidden'; // 배경 페이지 스크롤 막기
+}
+
+// 모달 닫기
+function closeModal3() {
+    document.getElementById("myModal3").style.display = "none"; /* 모달 창을 숨깁니다. */
+    document.body.style.overflow = 'auto'; // 배경 페이지 스크롤 허용
+}
+// 모달 열기
+function openModal4(clickedMenuId) {
+    const modalMenus = document.getElementsByClassName('modal_nav');
+    for (let i = 0; i < modalMenus.length; i++) {
+        modalMenus[i].style.textDecorationLine = 'none';
+    }
+    const modalContents = document.querySelectorAll('.v-content');
+    modalContents.forEach(content => {
+        content.style.display = 'none';
+    });
+    document.getElementById("myModal4").style.display = "block"; /* 모달 창을 보이게 합니다. */
+    document.getElementById("modal_nav4_1").style.textDecorationLine = 'underline';
+    document.getElementById("modal_nav4_1").style.textUnderlineOffset = '6px';
+    document.getElementById("modal_nav4_1").style.textDecorationThickness = '8px';
+    document.getElementById("modalMenusContent4_1").style.display = 'block';
+    document.body.style.overflow = 'hidden'; // 배경 페이지 스크롤 막기
+}
+
+// 모달 닫기
+function closeModal4() {
+    document.getElementById("myModal4").style.display = "none"; /* 모달 창을 숨깁니다. */
+    document.body.style.overflow = 'auto'; // 배경 페이지 스크롤 허용
+}
+// 모달 열기
+function openModal5(clickedMenuId) {
+    const modalMenus = document.getElementsByClassName('modal_nav');
+    for (let i = 0; i < modalMenus.length; i++) {
+        modalMenus[i].style.textDecorationLine = 'none';
+    }
+    const modalContents = document.querySelectorAll('.v-content');
+    modalContents.forEach(content => {
+        content.style.display = 'none';
+    });
+    document.getElementById("myModal5").style.display = "block"; /* 모달 창을 보이게 합니다. */
+    document.getElementById("modal_nav5_1").style.textDecorationLine = 'underline';
+    document.getElementById("modal_nav5_1").style.textUnderlineOffset = '6px';
+    document.getElementById("modal_nav5_1").style.textDecorationThickness = '8px';
+    document.getElementById("modalMenusContent5_1").style.display = 'block';
+    document.body.style.overflow = 'hidden'; // 배경 페이지 스크롤 막기
+}
+
+// 모달 닫기
+function closeModal5() {
+    document.getElementById("myModal5").style.display = "none"; /* 모달 창을 숨깁니다. */
+    document.body.style.overflow = 'auto'; // 배경 페이지 스크롤 허용
+}
+// 모달 열기
+function openModal6(clickedMenuId) {
+    const modalMenus = document.getElementsByClassName('modal_nav');
+    for (let i = 0; i < modalMenus.length; i++) {
+        modalMenus[i].style.textDecorationLine = 'none';
+    }
+    const modalContents = document.querySelectorAll('.v-content');
+    modalContents.forEach(content => {
+        content.style.display = 'none';
+    });
+    document.getElementById("myModal6").style.display = "block"; /* 모달 창을 보이게 합니다. */
+    document.getElementById("modal_nav6_1").style.textDecorationLine = 'underline';
+    document.getElementById("modal_nav6_1").style.textUnderlineOffset = '6px';
+    document.getElementById("modal_nav6_1").style.textDecorationThickness = '8px';
+    document.getElementById("modalMenusContent6_1").style.display = 'block';
+    document.body.style.overflow = 'hidden'; // 배경 페이지 스크롤 막기
+}
+
+// 모달 닫기
+function closeModal6() {
+    document.getElementById("myModal6").style.display = "none"; /* 모달 창을 숨깁니다. */
+    document.body.style.overflow = 'auto'; // 배경 페이지 스크롤 허용
+}
+// 모달 열기
+function openModal7(clickedMenuId) {
+    const modalMenus = document.getElementsByClassName('modal_nav');
+    for (let i = 0; i < modalMenus.length; i++) {
+        modalMenus[i].style.textDecorationLine = 'none';
+    }
+    const modalContents = document.querySelectorAll('.v-content');
+    modalContents.forEach(content => {
+        content.style.display = 'none';
+    });
+    document.getElementById("myModal7").style.display = "block"; /* 모달 창을 보이게 합니다. */
+    document.getElementById("modal_nav7_1").style.textDecorationLine = 'underline';
+    document.getElementById("modal_nav7_1").style.textUnderlineOffset = '6px';
+    document.getElementById("modal_nav7_1").style.textDecorationThickness = '8px';
+    document.getElementById("modalMenusContent7_1").style.display = 'block';
+    document.body.style.overflow = 'hidden'; // 배경 페이지 스크롤 막기
+}
+
+// 모달 닫기
+function closeModal7() {
+    document.getElementById("myModal7").style.display = "none"; /* 모달 창을 숨깁니다. */
+    document.body.style.overflow = 'auto'; // 배경 페이지 스크롤 허용
+}
+// 모달 열기
+function openModal8(clickedMenuId) {
+    const modalMenus = document.getElementsByClassName('modal_nav');
+    for (let i = 0; i < modalMenus.length; i++) {
+        modalMenus[i].style.textDecorationLine = 'none';
+    }
+    const modalContents = document.querySelectorAll('.v-content');
+    modalContents.forEach(content => {
+        content.style.display = 'none';
+    });
+    document.getElementById("myModal8").style.display = "block"; /* 모달 창을 보이게 합니다. */
+    document.getElementById("modal_nav8_1").style.textDecorationLine = 'underline';
+    document.getElementById("modal_nav8_1").style.textUnderlineOffset = '6px';
+    document.getElementById("modal_nav8_1").style.textDecorationThickness = '8px';
+    document.getElementById("modalMenusContent8_1").style.display = 'block';
+    document.body.style.overflow = 'hidden'; // 배경 페이지 스크롤 막기
+}
+
+// 모달 닫기
+function closeModal8() {
+    document.getElementById("myModal8").style.display = "none"; /* 모달 창을 숨깁니다. */
+    document.body.style.overflow = 'auto'; // 배경 페이지 스크롤 허용
+}
+// 모달 메뉴 스타일 적용 시작
+function modalMenuClick(clickedMenuId, contentId) {
+    const modalMenus = document.getElementsByClassName('modal_nav');
+    for (let i = 0; i < modalMenus.length; i++) {
+        modalMenus[i].style.textDecorationLine = 'none';
+    }
+    const clickedMenu = document.getElementById(clickedMenuId);
+    clickedMenu.style.textDecorationLine = 'underline';
+    clickedMenu.style.textUnderlineOffset = '6px';
+    clickedMenu.style.textDecorationThickness = '8px';
+
+    // 모달 메뉴에 따라 내용 변경 파트
+    const modalContents = document.querySelectorAll('.v-content');
+    modalContents.forEach(content => {
+        content.style.display = 'none';
+    });
+    const clickedContent = document.getElementById(contentId);
+    clickedContent.style.display = 'block';
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+    document.body.style.overflow = 'auto'; // 배경 페이지 스크롤 허용
+}
+
+document.querySelectorAll('.modal').forEach(modal => {
+    modal.addEventListener('click', function (event) {
+        if (event.target.classList.contains('modal')) {
+            closeModal(modal.id);
+            document.body.style.overflow = 'auto'; // 배경 페이지 스크롤 허용
+        }
+    });
+});
+// 모달 메뉴에 따라 내용 변경 파트 끝
+// 이미지 슬라이드 시작
+document.addEventListener('DOMContentLoaded', () => {
+    const sliders = document.querySelectorAll('.slider');
+    
+    sliders.forEach(slider => {
+        const slides = slider.querySelector('.slides');
+        const slide = slider.querySelectorAll('.slide');
+        const prev = slider.querySelector('.prev');
+        const next = slider.querySelector('.next');
+
+        let index = 0;
+
+        function showSlide(i) {
+            if (i >= slide.length) {
+                index = 0;
+            } else if (i < 0) {
+                index = slide.length - 1;
+            } else {
+                index = i;
+            }
+            slides.style.transform = `translateX(-${index * 100}%)`;
+        }
+
+        prev.addEventListener('click', () => {
+            showSlide(index - 1);
+        });
+
+        next.addEventListener('click', () => {
+            showSlide(index + 1);
+        });
+
+        showSlide(index);
+    });
+});
+// 이미지 슬라이드 끝
